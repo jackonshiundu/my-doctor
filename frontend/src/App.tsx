c6e7a8b363cc17cebf761d369f6cbde8cba99165
jackonshiundu <jackonshiundu2019@gmail.com>
2025-02-11 17:11:06 +0300

    pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
-  return <div className="text-green-500">App</div>;
+  return (
+    <div className="mx-4 sm:mx-[10%]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
