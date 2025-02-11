0ea3f4c9801f5a113ea69f67a837aeca3b127f98
jackonshiundu <jackonshiundu2019@gmail.com>
2025-02-11 17:20:00 +0300

    routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Doctors from "./pages/Doctors";
+import Login from "./pages/Login";
+import Contact from "./pages/Contact";
+import MyAppointments from "./pages/MyAppointments";
+import MyProfile from "./pages/MyProfile";
+import Appointment from "./pages/Appointment";
 
 const App = () => {
   return (
     <div className="mx-4 sm:mx-[10%]">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/doctors/:speciality" element={<Doctors />} />
+        <Route path="/my-appointments" element={<MyAppointments />} />
+        <Route path="/appointment/:docid" element={<Appointment />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
