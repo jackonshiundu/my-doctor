6b55fc6807db808f34ca26509c9b9c24dcaafc23
jackonshiundu <jackonshiundu2019@gmail.com>
2025-02-27 13:46:50 +0300

    doctor-model

---
+++ backend/src/models/doctor.model.ts
@@ -0,0 +1,52 @@
+import mongoose, { ObjectId, Schema } from "mongoose";
+
+export interface TheDoctor extends Document {
+  _id: string;
+  name: string;
+  email: string;
+  password: string;
+  image: string;
+  speciality: string;
+  degree: string;
+  experiance: string;
+  available: boolean;
+  fee: number;
+  Address: {
+    line1: string;
+    line2: string;
+  };
+  date: number;
+  slots_booked: {
+    slot_id: string;
+    patient_id: string;
+    date: string;
+    time: string;
+  }[];
+}
+const doctorSchema: Schema<TheDoctor> = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    image: { type: String, required: true },
+    speciality: { type: String, required: true },
+    degree: { type: String, required: true },
+    experiance: { type: String, required: true },
+    available: { type: Boolean, required: true },
+    fee: { type: Number, required: true },
+    Address: { type: Object, required: true },
+    date: { type: Number, required: true },
+    slots_booked: [
+      {
+        slot_id: { type: String, required: true },
+        patient_id: { type: String, required: true },
+        date: { type: String, required: true },
+        time: { type: String, required: true },
+      },
+    ],
+  },
+  { minimize: false }
+);
+const doctorModel = mongoose.model("doctor", doctorSchema);
+
+export default doctorModel;
