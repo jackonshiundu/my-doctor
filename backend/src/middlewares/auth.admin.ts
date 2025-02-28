1b8ac6457fb291a4cc8c2c9087fdbe151ff1056e
jackonshiundu <jackonshiundu2019@gmail.com>
2025-02-28 13:00:15 +0300

    admin-auth-middleware

---
+++ backend/src/middlewares/auth.admin.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+//admin's authentication middleware
+
+const authAdmin = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { atoken } = req.headers;
+    if (!!atoken || Array.isArray(atoken)) {
+      res.status(400).json({
+        success: false,
+        message: "Not authorised",
+      });
+      return;
+    }
+    const decodedToken = jwt.verify(atoken, process.env.JWT_SECRET);
+    if (decodedToken !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
+      res.status(400).json({
+        success: false,
+        message: "Not valid token",
+      });
+      return;
+    }
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+export default authAdmin;
--- backend/src/routes/admin.route.ts
+++ backend/src/routes/admin.route.ts
@@ -2,10 +2,11 @@ import express from "express";
 
 import { addDoctor, loginAdmin } from "../controllers/admin.controller";
 import upload from "../middlewares/multer";
+import authAdmin from "../middlewares/auth.admin";
 
 const adminRouter = express.Router();
 
-adminRouter.post("/adddoctor", upload.single("image"), addDoctor);
+adminRouter.post("/adddoctor", authAdmin, upload.single("image"), addDoctor);
 adminRouter.post("/login-admin", loginAdmin);
 
 export default adminRouter;
