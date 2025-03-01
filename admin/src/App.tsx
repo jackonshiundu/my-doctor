b8f34d6ab76d716c3ebb3979e4755ee474bb5be3
jackonshiundu <jackonshiundu2019@gmail.com>
2025-03-01 20:42:02 +0300

    Lougout-and-navbar

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,17 +1,19 @@
 import { useContext } from "react";
 import Login from "./pages/Login";
 import { AdminContext } from "./context/AdminContext";
+import Navbar from "./componentss/Navbar";
 
 const App = () => {
   const { aToken } = useContext(AdminContext);
-  console.log(aToken);
 
   return !aToken ? (
     <div className="">
       <Login />
     </div>
   ) : (
-    <>hello</>
+    <div className="bg-gray-100">
+      <Navbar />
+    </div>
   );
 };
 
