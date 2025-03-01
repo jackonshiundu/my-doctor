e412a5c9ea781db5f00690c79ddf19aba9d20e5d
jackonshiundu <jackonshiundu2019@gmail.com>
2025-03-01 21:38:13 +0300

    side-bar

--- admin/src/componentss/Sidebar.tsx
+++ admin/src/componentss/Sidebar.tsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AdminContext } from "../context/AdminContext";
+import { NavLink } from "react-router-dom";
+import { assets } from "../assets/assets";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const { aToken } = useContext(AdminContext);
+  return (
+    <div className="min-h-screen border-r bg-white">
+      {aToken && (
+        <ul className="text-gray-600 mt-5">
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
+                isActive ? "bg-green-50 border-r-4 border-primary" : ""
+              }`
+            }
+            to="/admin-dashboard"
+          >
+            <img src={assets.home_icon} alt="home-icon" />
+            <p>Dashboard</p>
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
+                isActive ? "bg-green-50 border-r-4 border-primary" : ""
+              }`
+            }
+            to="/all-appointments"
+          >
+            <img src={assets.appointment_icon} alt="appointment-icon" />
+            <p>Appointments</p>
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
+                isActive ? "bg-green-50 border-r-4 border-primary" : ""
+              }`
+            }
+            to="/add-doctor"
+          >
+            <img src={assets.add_icon} alt="home-icon" />
+            <p>Add Doctor</p>
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
+                isActive ? "bg-green-50 border-r-4 border-primary" : ""
+              }`
+            }
+            to="/doctor-list"
+          >
+            <img src={assets.people_icon} alt="home-icon" />
+            <p>Doctors List</p>
+          </NavLink>
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default Sidebar;
