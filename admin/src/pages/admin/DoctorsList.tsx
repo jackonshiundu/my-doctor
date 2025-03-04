7320f4427a217e9f6afb3d1aa06f0de76105b65c
jackonshiundu <jackonshiundu2019@gmail.com>
2025-03-05 00:07:37 +0300

    Doctors-list-page

--- admin/src/pages/admin/DoctorsList.tsx
+++ admin/src/pages/admin/DoctorsList.tsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import { AdminContext } from "../../context/AdminContext";
+import { Doctor } from "../../assets/assets";
 
 const DoctorsList = () => {
   const { doctors, getAllDoctors, aToken } = useContext(AdminContext);
@@ -9,7 +10,35 @@ const DoctorsList = () => {
       getAllDoctors();
     }
   }, [aToken]);
-  return <div></div>;
+  return (
+    <div className="m-5 m-h-[90vh] overflow-y-scroll ">
+      <h1 className="text-lg font-medium">All Doctors</h1>
+      <div className="flex flex-wrap w-full gap-4 pt-5 gap-y-5">
+        {doctors.map((doctor: Doctor, index: number) => (
+          <div
+            className="border border-indigo-200 rounded-xl max-w-56 overflow-hidden cursor-pointer group"
+            key={index}
+          >
+            <img
+              className="bg-green-100 group-hover:bg-primary transition-all duration-500"
+              src={doctor.image}
+              alt={doctor.name}
+            />
+            <div className="p-4 ">
+              <p className="text-neutral-800 text-lg font-medium">
+                {doctor.name}
+              </p>
+              <p className="text-zinc-600 text-sm">{doctor.speciality}</p>
+              <div className="mt-2 flex items-center gap-1 text-sm">
+                <input type="checkbox" checked={doctor.available} />
+                <p>Available</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default DoctorsList;
