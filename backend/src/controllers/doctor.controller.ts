cc48320ecce53cdf6efdbea6bab5259f4f9b3942
jackonshiundu <jackonshiundu2019@gmail.com>
2025-03-05 20:24:57 +0300

    changing-availability-controller

--- backend/src/controllers/doctor.controller.ts
+++ backend/src/controllers/doctor.controller.ts
@@ -1,1 +1,16 @@
+import { Request, Response } from "express";
+import doctorModel from "../models/doctor.model";
 
+const chnageAvailablity = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.body;
+    const docData = await doctorModel.findById(id);
+    await doctorModel.findByIdAndUpdate(id, { available: !docData.available });
+    res.status(200).json({ seccess: true, message: "Availability Chnaged" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { chnageAvailablity };
--- backend/src/routes/admin.route.ts
+++ backend/src/routes/admin.route.ts
@@ -7,11 +7,13 @@ import {
 } from "../controllers/admin.controller";
 import upload from "../middlewares/multer";
 import authAdmin from "../middlewares/auth.admin";
+import { chnageAvailablity } from "controllers/doctor.controller";
 
 const adminRouter = express.Router();
 
 adminRouter.post("/adddoctor", authAdmin, upload.single("image"), addDoctor);
 adminRouter.post("/login-admin", loginAdmin);
 adminRouter.get("/all-doctors", authAdmin, getAllDoctors);
+adminRouter.patch("/change-availability", authAdmin, chnageAvailablity);
 
 export default adminRouter;
