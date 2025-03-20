89dd769863d540c66792b7a0b1d570568114cea6
jackonshiundu <jackonshiundu2019@gmail.com>
2025-03-20 22:45:42 +0300

    doctors-context

--- admin/src/context/DoctorContext.tsx
+++ admin/src/context/DoctorContext.tsx
@@ -1,9 +1,23 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-export const DoctorContext = createContext();
+interface DoctorContext {
+  backendUrl: string;
+  dToken: string | null;
+  setDToken: (aToken: string | null) => void;
+}
+const initialDoctorContext = {
+  backendUrl: "",
+  dToken: "",
+  setDToken: () => {},
+};
+export const DoctorContext = createContext<DoctorContext>(initialDoctorContext);
 
-const DoctorContextProvider = (props) => {
-  const value = {};
+const DoctorContextProvider = (props: any) => {
+  const [dToken, setDToken] = useState(
+    localStorage.getItem("dToken") ? localStorage.getItem("dToken") : ""
+  );
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const value = { backendUrl, dToken, setDToken };
   return (
     <DoctorContext.Provider value={value}>
       {props.children}
