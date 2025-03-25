80210883b83ff56408d2993a102b6838abb53eaf
jackonshiundu <jackonshiundu2019@gmail.com>
2025-03-25 16:45:48 +0300

    Loggingout the doctor

--- admin/src/componentss/Navbar.tsx
+++ admin/src/componentss/Navbar.tsx
@@ -2,15 +2,19 @@ import React, { useContext } from "react";
 import { assets } from "../assets/assets";
 import { AdminContext } from "../context/AdminContext";
 import { useNavigate } from "react-router-dom";
+import { DoctorContext } from "../context/DoctorContext";
 
 const Navbar = () => {
   const { aToken, setAToken } = useContext(AdminContext);
+  const { dToken, setDToken } = useContext(DoctorContext);
   const navigate = useNavigate();
   const logout = () => {
     navigate("/");
-    if (aToken) {
+    if (aToken || dToken) {
       setAToken("");
+      setDToken("");
       localStorage.removeItem("aToken");
+      localStorage.removeItem("dToken");
     }
   };
   return (
